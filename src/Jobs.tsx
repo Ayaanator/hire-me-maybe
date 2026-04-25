28595835bf63f7419035ab70f02dc9d338bd5b6e
Ayaan Adrito <ayaan.adrito@gmail.com>
2026-04-25 19:01:43 -0400

    add job search limit

--- src/Jobs.tsx
+++ src/Jobs.tsx
@@ -1,11 +1,9 @@
 import { useEffect, useState } from 'react';
 import jobs from './jobs.json'
-import { useInteract, useJob } from './store.js'
+import { useInteract, useJob, usePremium } from './store.js'
 import PremiumModal from './PremiumModal'
 import CreditModal from './CreditModal'
 import InteractModal from './InteractModal'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Apply from './Apply';
 
 function Jobs() {
   const jobState = useJob((s) => s.jobState);
@@ -13,6 +11,8 @@ function Jobs() {
   
   const selectedJob = jobs.find(job => job.id === jobState);
   const setInteractState = useInteract((state) => state.setInteractState);
+
+  const setPremiumState = usePremium((state) => state.setPremiumState);
 
 
   const [jobMetrics, setJobMetrics] = useState(() => {
@@ -54,7 +54,7 @@ function Jobs() {
   return (
   <div className="max-w-6xl mx-auto grid grid-cols-12  bg-[#fff9ed] items-start">
 
-    <div className="col-span-5 flex flex-col gap-3 bg-white  border-1 border-gray-300 border-t-0 border-r-0">
+    <div className="col-span-5 flex flex-col gap-3 bg-white  border-1 border-gray-300 border-t-0 border-r-1">
       <h1 className="text-2xl font-semibold p-2">Top job picks for you</h1>
       <h1 className="text-sm p-2">Based on your profile, search history, preferences, unpaid emotional labor, and your ability to click “I Agree” without reading anything</h1>
       <h1 className="text-sm p-2">314 results</h1>
@@ -67,7 +67,8 @@ function Jobs() {
             <div
               key={job.id}
               onClick={() => setJobState(job.id)}
-              className={`relative border border-gray-300 p-2 cursor-pointer
+              className={`relative border border-gray-300 p-2 cursor-pointer border-r-0 
+                ${job.id === 1 ? "border-t-1" : "border-t-0"}
                 ${isSelected ? "bg-gray-100" : ""}
               `}
             >
@@ -75,15 +76,17 @@ function Jobs() {
                 <div className="absolute left-0 top-0 h-full w-1 bg-black" />
               )}
 
-              <div className="flex flex-row gap-3">
+              <div className="flex flex-row gap-3 group">
                 <img
                   src={job.profile}
                   alt="profile"
-                  className="w-14 h-14 object-cover border border-gray-200"
+                  className="w-16 h-16 object-cover border border-gray-200"
                 />
                 <div>
-                  <h1>{job.title}</h1>
+                  <h1 className="text-blue-600 group-hover:underline font-semibold">{job.title}</h1>
                   <h2>{job.company}</h2>
+                  <h2 className="text-sm mt-3">{job.location}</h2>
+                  <h2 className="text-sm text-gray-500">{job.salary}</h2>
                 </div>
               </div>
             </div>
@@ -95,7 +98,7 @@ function Jobs() {
     <div className="col-span-7 flex flex-col gap-4 bg-white p-4 border border-gray-300 border-t-0 border-l-0">
       {!selectedJob ? (
         <h1 className="text-gray-500">Select a job to view details</h1>
-      ) : (
+      ) : selectedJob.description !== "PREMIUM_PAYWALL" ? (
         <>
           <div className="flex items-start justify-between gap-4">
             <div>
@@ -168,6 +171,21 @@ function Jobs() {
             <h1>{selectedJob.salary}</h1>
           </div>
         </>
+      ) : (
+        <div className="flex flex-col items-center justify-center text-center p-6">
+          <h1 className="text-xl font-semibold mb-2">
+            Daily Job Search Limit Reached
+          </h1>
+
+          <p className="text-sm text-gray-600 mb-4">
+            You’ve run out of free job searches for today. Upgrade to Premium to continue browsing opportunities without interruption.
+          </p>
+
+          <button onClick={() => setPremiumState("true")}
+          className="bg-[#ffd76b] text-black px-5 py-2 text-sm font-semibold rounded-full hover:bg-[#daa000] transition cursor-pointer">
+            Buy Premium
+          </button>
+        </div>
       )}
     </div>
     
